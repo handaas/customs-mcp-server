dbeac0ab9c1dcc2d75ea21c2c248a22f679c45e5
刹那芳华life <306862600@qq.com>
2025-06-28 11:15:40 +0800

    更新API调用返回值处理，优化查询结果；调整部分函数的默认参数值以提高可用性。

--- server/mcp_server.py
+++ server/mcp_server.py
@@ -60,7 +60,7 @@ def call_api(product_id: str, params: dict) -> dict:
     url = f'https://console.handaas.com/api/v1/integrator/call_api/{INTEGRATOR_ID}'
     try:
         response = requests.post(url, data=call_params)
-        return response.json().get("data", "查询为空")
+        return response.json().get("data", None) or response.json().get("msgCN", None)
     except Exception as e:
         return "查询失败"
     
@@ -164,7 +164,7 @@ def customs_bigdata_export_recruitment_profile(matchKeyword: str, keywordType: s
 
 
 @mcp.tool()
-def customs_bigdata_export_order_regions(matchKeyword: str, pageIndex: int = None, pageSize: int = None,
+def customs_bigdata_export_order_regions(matchKeyword: str, pageIndex: int = 1, pageSize: int = 10,
                          keywordType: str = None) -> dict:
     """
     该接口的功能是根据提供的企业标识信息（如企业名称、注册号等）查询该企业外贸订单的地理分布情况，输出包括订单地区列表、订单金额、订单数量、涉及的国家或地区、净重，以及分布地区的总数。此接口可用于企业管理系统中，帮助企业分析其国际市场的分布状况，评估各地区的订单贡献度，从而制定更加有效的市场拓展策略。
@@ -231,7 +231,7 @@ def customs_bigdata_overseas_certifications(matchKeyword: str, keywordType: str 
 
 
 @mcp.tool()
-def customs_bigdata_fuzzy_search(matchKeyword: str, pageIndex: int = None, pageSize: int = None) -> dict:
+def customs_bigdata_fuzzy_search(matchKeyword: str, pageIndex: int = 1, pageSize: int = None) -> dict:
     """
     该接口的功能是根据提供的企业名称、人名、品牌、产品、岗位等关键词模糊查询相关企业列表。返回匹配的企业列表及其详细信息，用于查找和识别特定的企业信息。
 
